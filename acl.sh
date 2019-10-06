f7a8662253e9aee2b01a779c3230fc33e10d6904
getsaarthak <55044834+getsaarthak@users.noreply.github.com>
2019-10-06 10:25:37 -0700

    Update acl.sh

--- acl.sh
+++ acl.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ansible-playbook web.yml > disk_usage_report.txt
+ansible nodes -a "df -h" > disk_usage_report.txt
+ansible-playbook web.yml
 ansible-playbook check_acl.yml > getfacl_output.txt
